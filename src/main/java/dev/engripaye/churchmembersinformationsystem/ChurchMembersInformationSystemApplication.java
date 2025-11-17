620552a38e406310d45eb6d8624e4586f4d9385b
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-17 19:06:01 +0100

    init commit

---
+++ src/main/java/dev/engripaye/churchmembersinformationsystem/ChurchMembersInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.engripaye.churchmembersinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchMembersInformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchMembersInformationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/engripaye/churchmembersinformationsystem/ChurchMembersInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engripaye.churchmembersinformationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchMembersInformationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
