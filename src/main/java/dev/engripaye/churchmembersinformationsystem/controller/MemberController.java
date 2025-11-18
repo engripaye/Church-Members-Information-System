a803b1d2687b9c79b03d272a0562d8923b13ed18
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-18 20:26:40 +0100

    backend code commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/churchmembersinformationsystem/controller/MemberController.java
+++ src/main/java/dev/engripaye/churchmembersinformationsystem/controller/MemberController.java
@@ -1,10 +1,14 @@
 package dev.engripaye.churchmembersinformationsystem.controller;
 
 
+import dev.engripaye.churchmembersinformationsystem.dto.MemberRequest;
+import dev.engripaye.churchmembersinformationsystem.model.Member;
 import dev.engripaye.churchmembersinformationsystem.repository.MemberRepository;
 import dev.engripaye.churchmembersinformationsystem.service.MemberService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/members")
@@ -18,5 +22,31 @@ public class MemberController {
     }
 
 
+    @PostMapping
+    public ResponseEntity<Member> createMember(@RequestBody MemberRequest request){
+        return ResponseEntity.ok(memberService.createMember(request));
+
+    }
+
+    @GetMapping
+    public ResponseEntity <List<Member>> getAllMembers(){
+        return ResponseEntity.ok(memberService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Member> getMemberById(@PathVariable Long id){
+        Member member = memberService.getById(id);
+        return (member != null)
+                ? ResponseEntity.ok(member)
+                : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        boolean deleted = memberService.deleteById(id);
+        return deleted
+                ? ResponseEntity.noContent().build()
+                : ResponseEntity.notFound().build();
+    }
 
 }
