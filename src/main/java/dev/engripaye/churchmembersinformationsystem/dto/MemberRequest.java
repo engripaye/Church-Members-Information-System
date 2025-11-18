9c8e0988417ad5d267104099a00484f665b2b36c
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-18 13:42:56 +0100

    backend code commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/churchmembersinformationsystem/dto/MemberRequest.java
+++ src/main/java/dev/engripaye/churchmembersinformationsystem/dto/MemberRequest.java
@@ -119,9 +119,6 @@ public class MemberRequest {
                 this.address,
                 this.suggestion,
                 this.prayerPoint
-
-
-
         );
     }
 }
---
+++ src/main/java/dev/engripaye/churchmembersinformationsystem/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package dev.engripaye.churchmembersinformationsystem.repository;
+
+import dev.engripaye.churchmembersinformationsystem.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+
+}
