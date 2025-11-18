f002d9027c61cb11e7476a12d3ae6648e0c13b18
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-11-18 20:30:29 +0100

    backend code commit by ENGR. IPAYE

--- src/main/java/dev/engripaye/churchmembersinformationsystem/service/GoogleSheetService.java
+++ src/main/java/dev/engripaye/churchmembersinformationsystem/service/GoogleSheetService.java
@@ -1,10 +1,20 @@
 package dev.engripaye.churchmembersinformationsystem.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GoogleSheetService {
 
 
+    @Value("${google.sheet.id}")
+    private String sheetId;
+
+    @Value("${google.service.account.key}")
+    private String credentialsPath;
+
+    public Sheets GetSheetsService(){
+
+    }
 
 }
